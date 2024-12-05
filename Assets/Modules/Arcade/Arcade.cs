1cd2af1a149f985f2820c9f67bc5f0a0b0ac4ac1
Hugo Jette <hugo.jette3@gmail.com>
2024-12-05 12:58:22 -0500

    Arcade

---
+++ Assets/Modules/Arcade/Arcade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Arcade : MonoBehaviour, IInteractable
+{
+    [SerializeField] Camera ArcadeCamera;
+    [SerializeField] AudioLowPassFilter AudioFilter;
+
+    /// <inheritdoc/>
+    public void OnClick() => StartCoroutine(Start2DGame());
+
+
+    public IEnumerator Start2DGame()
+    {
+        Debug.Log("activated");
+        AudioFilter.enabled = true;
+        var loadScene = SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+
+        while (!loadScene.isDone)
+            yield return null;
+    }
+
+}
